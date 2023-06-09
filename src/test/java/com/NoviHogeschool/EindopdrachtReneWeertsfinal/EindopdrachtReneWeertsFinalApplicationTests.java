cff96757bebaa87ba7ab1f55a331ba4158fbec04
“rene-weerts” <rjmwrt@gmail.com>
2023-06-09 17:19:33 +0200

    initial commit

---
+++ src/test/java/com/NoviHogeschool/EindopdrachtReneWeertsfinal/EindopdrachtReneWeertsFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NoviHogeschool.EindopdrachtReneWeertsfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EindopdrachtReneWeertsFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
